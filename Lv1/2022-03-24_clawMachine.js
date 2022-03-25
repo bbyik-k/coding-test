2e0a2ff0aed7b2d7d547af75d6714d8202f2714e
bbyik <kby00517@naver.com>
2022-03-25 13:34:57 +0900

    Add LV1 clawMachine

--- Lv1/2022-03-24_clawMachine.js
+++ Lv1/2022-03-24_clawMachine.js
@@ -1,40 +1,3 @@
-function solution(board, moves) {
-  var stock = new Array;
-  var loss = 0;
-
-  for (var i = 0; i < moves.length; i++) {
-    var location = moves[i] - 1;
-    for (var k = 0; k < 5; k++) {
-      if (board[k][location] > 0) {
-        var doll = board[k][location];
-        stock.push(doll);
-        board[k][location] = 0;
-        break;
-      }
-
-      if (stock.length > 1) {
-        var last = stock[stock.length - 1];
-        var last_before = stock[stock.length - 2];
-        if (last == last_before) {
-          stock.pop();
-          stock.pop();
-          console.log("stock === " + stock);
-
-          loss = loss + 2
-        }
-
-      }
-
-    }
-
-  }
-  console.log("stock2 === " + stock);
-
-
-  var answer = loss;
-  return answer;
-}
-
 const board = [
   [0, 0, 0, 0, 0],
   [0, 0, 1, 0, 3],
@@ -42,7 +5,38 @@ const board = [
   [4, 2, 4, 4, 2],
   [3, 5, 1, 3, 1]
 ];
-
 const moves = [1, 5, 3, 5, 1, 2, 1, 4];
 
-console.log(solution(board, moves));
+const board2 = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 5],
+  [0, 3, 2, 0, 1],
+  [4, 3, 4, 4, 3],
+  [5, 2, 1, 5, 1]
+];
+// const moves2 = [1, 5, 1, 3, 2, 1, 2];
+const moves2 = [2, 2, 2, 3, 2, 1, 2];
+
+
+function solution(board, moves) {
+  let answer = 0;
+  let basket = [];
+  moves.forEach(move => {
+    for (let row of board) {
+      console.log(`row[${move-1}]: ${row[move - 1]}`);
+      if (row[move - 1] !== 0) {
+        if (row[move - 1] === basket[basket.length - 1]) {
+          basket.pop();
+          answer += 2;
+        } else {
+          basket.push(row[move - 1]);
+        }
+        row[move - 1] = 0;
+        break;
+      }
+    }
+  });
+
+  return answer;
+}
+console.log(solution(board2, moves2));
